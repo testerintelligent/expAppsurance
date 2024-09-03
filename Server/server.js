cdb51da4f7527d678d313a599679ccb0b7b54c97
Mageshpoopathi <mageshpoopathi208@gmail.com>
2024-09-03 13:21:20 +0530

    change DB URL

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-const mongoURI = 'mongodb://admin:admin@192.168.99.141:27017/registeredUser?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.0'; // Replace 'mydatabase' with your database name
+const mongoURI = 'mongodb://192.168.99.141:27017/registeredUser?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.0'; // Replace 'mydatabase' with your database name
 
 mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB connected successfully'))
