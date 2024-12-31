f0b34aa363d0313ec7e385774c0af542889d50cb
Mageshpoopathi <mageshpoopathi208@gmail.com>
2024-12-31 12:51:30 +0530

    Change Database IP address

--- Server/Models/DB.js
+++ Server/Models/DB.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/registeredUser?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.0'; // Replace 'mydatabase' with your database name
+const DatabaseIP=process.env.IPaddress;
+const mongoURI = `mongodb://${DatabaseIP}:27017/registeredUser?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.0`; // Replace 'mydatabase' with your database name
 
 mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB connected successfully'))
