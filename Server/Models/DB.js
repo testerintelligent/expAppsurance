4e25ce6e105f25fe7ae2fb62cfaaa71babc62bdb
Gurumoorthi <testerintelligent@2gmail.com>
2025-02-13 14:53:21 +0530

    new IP updated

--- Server/Models/DB.js
+++ Server/Models/DB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const DatabaseIP=process.env.IPaddress;
-const mongoURI = "mongodb://192.168.99.141:27017/registeredUser?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.0"
+const mongoURI = "mongodb://10.192.190.148:27017/registeredUser?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.0"
 
 mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB connected successfully'))
