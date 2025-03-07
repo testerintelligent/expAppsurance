347ecbb36f6bdf95f07f616170bd3dcedebd76d5
Mageshpoopathi <mageshpoopathi208@gmail.com>
2025-03-07 12:50:58 +0530

    commit Billing application too

---
+++ billing/src/App.js
@@ -0,0 +1,12 @@
+import "./style/tailwind.css";
+import BillingPage from "./Component/BillingPage";
+
+function App() {
+  return (
+    <div className="App">
+      <BillingPage/>
+    </div>
+  );
+}
+
+export default App;
