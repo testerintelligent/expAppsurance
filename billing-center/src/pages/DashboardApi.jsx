d60366dd5eacb5b5ade3dfba7e9c45138d9e0654
Geetha-Nagarajan <Geetha.Nagarajan@expleogroup.com>
2026-02-04 14:53:40 +0530

    Done with policy details table

--- billing-center/src/pages/DashboardApi.jsx
+++ billing-center/src/pages/DashboardApi.jsx
@@ -3,11 +3,12 @@ import axios from "axios";
 
 // const API_BASE_URL = "http://10.192.190.158:5000/api/accounts";
 
-export const searchPolicy = async ({ policyNumber }) => {
+export const searchPolicy = async (policyNumber) => {
   try {
     const response = await axios.get(
       `http://10.192.190.158:5000/api/Policies/getPolicyByNumber/${policyNumber}`
     );
+
     return response.data;
   } catch (err) {
     console.log("Failed to fetch policy details");
