21c7afd913ca611b493cd076cd788d16da7eeecc
Geetha-Nagarajan <Geetha.Nagarajan@expleogroup.com>
2026-03-03 16:49:40 +0530

    Get all Invoice and Get Invoice by Policy Number apis created

--- Server/Controller/InvoiceController.js
+++ Server/Controller/InvoiceController.js
@@ -1,4 +1,5 @@
 const Invoice = require("../Models/InvoiceDetails");
+const Policy = require("../Models/PolicyDetails");
 
 exports.createInvoice = async (req, res) => {
   try {
@@ -20,4 +21,47 @@ exports.createInvoice = async (req, res) => {
       message: err.message,
     });
   }
+};
+
+exports.getAllInvoice = async (req, res) => {
+  try {
+    // Fetch all policies from the database
+    const invoices = await Invoice.find({});
+
+    // Map policies to return only relevant information
+    const invoiceData = invoices.map((invoice) => ({
+      policyNumber: invoice.policyNumber,
+      productType: invoice.productType,
+      effectiveDate: invoice.effectiveDate,
+      expiryDate: invoice.expiryDate,
+      totalPremium: invoice.totalPremium,
+      totalCost: invoice.totalCost,
+      status: "Pending",
+      // Add other fields if needed, like customer info from other collections
+    }));
+
+    res.status(200).json({
+      message: "Invoice fetched successfully",
+      invoices: invoiceData,
+    });
+  } catch (err) {
+    console.error("Error fetching invoice:", err);
+    res.status(500).json({ message: "Error fetching invoice" });
+  }
+};
+
+exports.getInvoiceByPolicyNumber = async (req, res) => {
+  const { policyNumber } = req.params;
+
+  const invoice = await Invoice.findOne({ policyNumber });
+  // const invoice = await Policy.findOne({ policyNumber }).populate("policyId");
+  // .populate("contactId");
+
+  if (!invoice) {
+    return res.status(404).json({ message: "Invoice not found" });
+  }
+
+  res.json({
+    invoice,
+  });
 };
--- billing-center/src/pages/Invoice.jsx
+++ billing-center/src/pages/Invoice.jsx
@@ -1,30 +1,57 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import axios from "axios";
 
 const Invoice = () => {
-  const API_BASE_URL = "http://localhost:5000/api/billing";
-  const handleClick = async () => {
-    try {
-      const res = await axios.post(`${API_BASE_URL}/createInvoice`, {
-        totalCost: 1200,
-        policyNumber: 10,
-        productType: "Auto",
-        effectiveDate: "2026-01-28T00:00:00.000Z",
-        expiryDate: "2027-01-28T00:00:00.000Z",
-        totalPremium: 2850,
-        status: "Pending",
-      });
-
-      console.log("invoice", res.data);
-    } catch (error) {
-      console.error("Error:", error.response?.data || error.message);
-    }
-  };
   return (
-    <div>
-      <Button onClick={handleClick}>CREATE INVOICE </Button>
-    </div>
+    <Box sx={{ maxWidth: 1250, mx: "auto", py: 4 }}>
+      <Typography>Hello Invoice</Typography>
+      {/* <Paper
+        elevation={0}
+        sx={{
+          p: 2,
+          mb: 4,
+          display: "flex",
+          justifyContent: "space-between",
+          background: "#eaf4ff",
+          borderRadius: 2,
+          border: "1px solid #d0e4ff",
+          fontSize: "14px",
+        }}
+      >
+        <Typography>
+          <b>Submission:</b> {submissionId}
+        </Typography>
+        <Typography>
+          <b>Account:</b> {accountNumber}
+        </Typography>
+        <Typography>
+          <b>Policyholder:</b> {policyHolder}
+        </Typography>
+        <Typography>
+          <b>Effective Date:</b> {effectiveDate}
+        </Typography>
+      </Paper> */}
+    </Box>
   );
 };
 
 export default Invoice;
+
+// const API_BASE_URL = "http://localhost:5000/api/billing";
+// const handleClick = async () => {
+//   try {
+//     const res = await axios.post(`${API_BASE_URL}/createInvoice`, {
+//       totalCost: 1200,
+//       policyNumber: 10,
+//       productType: "Auto",
+//       effectiveDate: "2026-01-28T00:00:00.000Z",
+//       expiryDate: "2027-01-28T00:00:00.000Z",
+//       totalPremium: 2850,
+//       status: "Pending",
+//     });
+
+//     console.log("invoice", res.data);
+//   } catch (error) {
+//     console.error("Error:", error.response?.data || error.message);
+//   }
+// };
