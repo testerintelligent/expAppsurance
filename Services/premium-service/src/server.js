cda16a908dce7600540bce7fd0266045e4d15f01
shilpaexpleo <shilpa.kannan@expleogroup.com>
2026-04-03 12:37:48 +0530

    DB connection check

--- Services/premium-service/src/server.js
+++ Services/premium-service/src/server.js
@@ -6,12 +6,20 @@ const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5001;
 
-// Connect DB
-connectDB();
+
 
 app.get("/", (req, res) => {
   res.send("Premium Service Running");
 });
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ MongoDB connected successfully"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 
 // Start server
