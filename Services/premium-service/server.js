2ec7b5ff28f9911726d186fb722d5dd152f60870
shilpaexpleo <shilpa.kannan@expleogroup.com>
2026-04-06 14:53:02 +0530

    Testing the new server DB connection

--- Services/premium-service/server.js
+++ Services/premium-service/server.js
@@ -13,13 +13,7 @@ app.get("/", (req, res) => {
 });
 
 // MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("✅ MongoDB connected successfully"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+connectDB();
 
 
 // Start server
